b1e289976de6099a94b83ebd9839b211be6fcdfa
toan <toan.dq@orai.io>
2024-11-29 12:05:27 +0700

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export * from './initialize.js';
+export * from './initialize';
+export * from './solana';
+export * from './wallet';
+export * from './account';
+export * from './icon';
+export * from './window';
